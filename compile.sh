f24309f8a9a5d80e4af6b946a7cc8d8d534efc6b
Diego Montalvo <dmontalvo.m@gmail.com>
2017-05-15 10:37:13 -0500

    Primer commit de project LaTeX de tesis de Robots

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#!/bin/csh
+
+set intexfile   = Tesis    # intexfile
+
+rm $intexfile.ps $intexfile.dvi $intexfile.aux $intexfile.log
+
+latex $intexfile
+bibtex $intexfile
+latex $intexfile
+latex $intexfile
+
+dvips $intexfile.dvi -o
+ps2pdf $intexfile.ps $intexfile.pdf
+rm $intexfile.dvi $intexfile.aux $intexfile.log
